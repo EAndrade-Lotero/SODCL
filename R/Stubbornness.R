c719f9a5117896387e21a614fb49c7adf70d5bdd
Slendercoder <andradeed78@gmail.com>
2020-05-29 09:10:34 -0500

    R code to check stubbornness in human data

---
+++ R/Stubbornness.R
@@ -0,0 +1,24 @@
+library(sjPlot)
+library(sjmisc)
+library(ggplot2)
+
+df1 = read.csv("../Data/humans_only_absent.csv")
+# head(df1)
+
+df1 <- df1[complete.cases(df1), ]
+
+# Regressing Consistency w.r.t. max similarity to focal region
+model4h <- lm(Consistency ~ Similarity_LAG1, data = df1)
+summary(model4h) # => Positive correlation is significant
+
+g3 <- ggplot(df1, aes(Similarity_LAG1, Consistency)) +
+  geom_point(alpha = 1/8) +
+  xlab("Log of max similarity w.r.t.\nfocal regions on Round n-1") +
+  ylab("Consistency on Round n") +
+  geom_smooth(method = lm)
+
+g3 <- g3 + theme_sjplot()
+
+g3 
+
+ggsave("ConsistencyWRTDist2FR.pdf", width=2, height=2, dpi=1200, g3)
