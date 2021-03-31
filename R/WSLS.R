1cf907bde88da671c19c847599c05002cd2f1913
Slendercoder <edgar@Edgars-Mac-mini.local>
2021-03-31 16:43:53 -0500

    R code to check WSLS heuristics on human data

--- R/WSLS.R
+++ R/WSLS.R
@@ -1,9 +1,13 @@
+library(sjmisc)
 library(ggplot2)
 
 df1 = read.csv("../Data/humans_only_absent.csv")
 # head(df1)
 
 df1 <- df1[complete.cases(df1), ]
+
+# Pearson correlation
+cor(df1$Consistency, df1$Score_LAG1) # => 0.23
 
 # Regressing Consistency(n) w.r.t. Score(n-1)
 model1h <- lm(Consistency ~ Score_LAG1, data = df1)
@@ -14,8 +18,9 @@ g1 <- ggplot(df1, aes(Score_LAG1, Consistency)) +
   theme_bw() +
   xlab("Score(n-1)") +
   ylab("Consistency(n)") +
-  geom_smooth(method = lm)
+  stat_smooth(method='lm', formula = y~poly(x,1))
+  # geom_smooth(method = lm)
 
 g1
 
-ggsave("ConsistencyWRTScore.pdf", width=2, height=2, dpi=1200, g1)
+ggsave("ConsistencyWRTScore.png", width=2, height=2, dpi=600, g1)
